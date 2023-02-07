44869f9532bfbdfc05b46c0c77cf0d60711b0f88
ajorgensen89 <ajorgensen89@gmail.com>
2023-02-07 21:00:39 +0000

    Sort Errors for name input

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from random import randint
 
 
-def name_input():
+def get_name_input():
     """
-    Create name for use's Battleship Board. Check for input and 
-    raise an error if no 'name' has been added to input.
+    Create name for user's Battleship Board. 
+    Run while loop to check user had entered a name for their boardgame.
+    Break loop if valid 'input' has been added. Continue with the game.
+    This can be numbers, letters or special characters.
+    For example: Player1 or Bobby. Even @Bobby_BattleShip-game!
     """
     while True:
         print("="*25)
@@ -16,50 +19,63 @@ def name_input():
 
         name_input = input("Enter a name:")
         name = name_input
-        if name:
-            print_name = (name_input + "'s board!")
+        if validate_input(name):
+            print_name = (name + "'s board!")
             print(print_name)
             break
-            
+    return name        
+   
+
+def validate_input(value):    
+    """
+    Validates user input for name. 
+    Raises Error if User had not added a name for their Battleship board.
+    Check for input in try:
+    Uses except: To print out Error.
+    """  
     try:
-        if not name:
+        if not value:
             raise EOFError(
                 "Not Valid... You need to enter a name."
             )              
     except EOFError as e:
         print(f"Your input caused an error: {e} Try again.")
         return False
-     
-    return True    
-    
-    
-name_input() 
 
+    return True 
+
+
+data = get_name_input() 
+print(data, "gagaga")
 
 # Create empty list to store the game's board.
 board = []
-# Create a list of 5 lists, all 5 "o" characters long. Add in to
+
+# Create a list of 5 lists, all 5 "o" characters long. Loop and add in to
 # the empty board variable above.
 for x in range(5):
     board.append(["o"] * 5)
 
-# Create a function that combines 5 lists into a 5x5 grid shape 
-# and remove decoration.
-# Join list with a 'space' between the "o" characters.
+# Define a Battleship Board function in board_game
 
 
-def board_game(board):  
+def board_game(board):
+    """
+    Create a function that combines the above 5 lists into a 5x5 grid shape 
+    and remove decoration.
+    Join list with a 'space' between the "o" characters.
+    """  
     for row in board:
         print((" ").join(row))
+
+# Place one ship randomly on the grid
 
 
 board_game(board)
 
-# Place one ship randomly on the grid
-random_row = randint(0, len(board) - 1)
-random_column = randint(0, len(board[0]) - 1)
-
-# Place five shipd randomly on the grid.
+# Prints one Battleship 'S', randomly on the board
+random_row = randint(0, len(board))
+random_column = randint(0, len(board[0]))
 
 # Prints ship loction. To be removed.
 print(random_row, random_column)
@@ -71,20 +87,17 @@ guess_column = int(input("GUESS COLUMN:\n"))
 if guess_row == random_row and guess_column == random_column:
     print("HIT. Marked with a '*' on the board")
     print(f"The Battleship sunk on Row:{guess_row} and Column:{guess_column}")
+    print(data, "'s Board")
     board[guess_row][guess_column] = "*"
     print(board_game(board))
 else:
     print("MISSED. Marked with an 'X' on the board")
     print(f"It was on Row:{random_row} and Column:{random_column}")
     print("The Battleship's position is marked with an 'S' on the board")
-
+    print(data, "'s Board")
     board[random_row][random_column] = "S"
     board[guess_row][guess_column] = "X" 
     print(board_game(board)) 
-
-
-# Place one ship randomly on the grid.
-# ships = []
 
 
 # def get_five_ships(ships) -> int:
