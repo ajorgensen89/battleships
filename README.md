79a2d3d2f00b932b388af346d3759f8ee8ea3dd2
ajorgensen89 <ajorgensen89@gmail.com>
2023-02-08 00:30:19 +0000

    Develop Error messsages for input for row and column guessed by user. Add evidence to README.md file for playing game and Errors encountered

--- README.md
+++ README.md
@@ -6,50 +6,11 @@ Trial and Errors.<br>
 Tried using Append() method to add a HIT or MISS to the Battleship Board but it was not working correctly as I had not identified the user's guess for row and column are in separate lists. It lead to it just being added to the list and not changing the current "o" character which makes the hidden board.<br>
 ![Append atempt](images/readme-images/Append.board.png)
 
+HIT.<br>
+Method for showing a hit on a Battleship in combat.<br>
+![HIT image](images/readme-images/HITexample.png)
+
+
 # Create empty list to store the game's board.
 board = []
 computer = []
-
-# Create a list of 5 lists, all 5 "o" characters long. Loop and add in to
-# the empty board variable above.
-for x in range(5):
-    board.append(["o"] * 5)
-
-
-def board_game(board):
-    """
-    Create a function that combines the above 5 lists into a 5x5 grid shape 
-    and remove decoration.
-    Join list with a 'space' between the "o" characters.
-    """  
-    for row in board:
-        print((" ").join(row))
-
-# Place one ship randomly on the grid
-
-
-board_game(board)
-
-random_row = randint(0, len(board))
-random_column = randint(0, len(board[0]))
-
-# Prints ship loction. To be removed.
-print(random_row, random_column)
-
-# Input from the user for row and column choice.
-guess_row = int(input("GUESS ROW:\n"))
-guess_column = int(input("GUESS COLUMN:\n"))
-
-if guess_row == random_row and guess_column == random_column:
-    print("HIT. Marked with a '*' on the board")
-    print(f"The Battleship sunk on Row:{guess_row} and Column:{guess_column}")
-    board[guess_row][guess_column] = "*"
-    print(board_game(board))
-else:
-    print("MISSED. Marked with an 'X' on the board")
-    print(f"It was on Row:{random_row} and Column:{random_column}")
-    print("The Battleship's position is marked with an 'S' on the board")
-
-    board[random_row][random_column] = "S"
-    board[guess_row][guess_column] = "X" 
-    print(board_game(board)) 
--- run.py
+++ run.py
@@ -1,52 +1,100 @@
 from random import randint
+import random
 
 
 def get_name_input():
     """
-    Create name for user's Battleship Board. 
+    Create name for user's Battleship Board.
     Run while loop to check user had entered a name for their boardgame.
     Break loop if valid 'input' has been added. Continue with the game.
     This can be numbers, letters or special characters.
     For example: Player1 or Bobby. Even @Bobby_BattleShip-game!
     """
     while True:
+        # Welcoming message with a few instructions to play.
         print("="*25)
         print("WELCOME TO BATTLESHIPS")
         print("Select a row: 0 to 4")
         print("and")
         print("Select a column: 0 to 4")
         print("=" * 25)
-
+        # Create input field for a user to create a name or use their own.
         name_input = input("Enter a name:")
         name = name_input
+        # If a valid input is filled in, break from while loop and continue
+        # with the Battleship game.
         if validate_input(name):
-            print_name = (name + "'s board!")
+            print_name = name + "'s board!"
             print(print_name)
             break
-    return name        
-   
+    # Return name value, to function, to be used to label the user's board.
+    # Saved in variable - data.
+    return name
 
-def validate_input(value):    
+
+def validate_input(value):
     """
-    Validates user input for name. 
-    Raises Error if User had not added a name for their Battleship board.
+    Validates user input for name.
+    Raises Error if user has not added a name for their Battleship board.
     Check for input in try:
     Uses except: To print out Error.
-    """  
+    """
     try:
         if not value:
             raise EOFError(
                 "Not Valid... You need to enter a name."
-            )              
-    except EOFError as e:
-        print(f"Your input caused an error: {e} Try again.")
+            )
+    except EOFError as e_err:
+        print(f"Your input caused an error: {e_err} Try again.")
         return False
 
-    return True 
+    return True
+
+# Saved name input for players board to be named in data variable.
 
 
-data = get_name_input() 
-print(data, "gagaga")
+data = get_name_input()
+
+
+# Storage variables for guesssing a row and column.
+guess_row = int(input("GUESS ROW:\n"))
+guess_column = int(input("GUESS COLUMN:\n"))
+
+
+def player_guess():
+    """
+    To validate guess for row and column by user.
+    """
+    while True:
+        # Input from the user for row and column choice.
+        if validate_player_guess(guess_row):
+            print("Working...")
+            break
+        if validate_player_guess(guess_column):
+            print("working too..")
+            break
+        return guess_row, guess_column
+
+
+def validate_player_guess(guessed):
+    """
+    Errors for grid ranges. Raise error if out of range of the board length.
+    """
+    try:
+        if guessed >= 5:
+            raise ValueError(
+                "Out of grid range! Try again."
+            )
+    except ValueError as v_err:
+        print(f"Oops. {v_err}")
+        return False
+
+    return True
+
+
+decision = player_guess()
+print("Incorrect Enteries:", decision)
+
 
 # Create empty list to store the game's board.
 board = []
@@ -61,28 +109,21 @@ for x in range(5):
 
 def board_game(board):
     """
-    Create a function that combines the above 5 lists into a 5x5 grid shape 
+    Create a function that combines the above 5 lists into a 5x5 grid shape
     and remove decoration.
     Join list with a 'space' between the "o" characters.
-    """  
+    """
     for row in board:
         print((" ").join(row))
 
 # Place one ship randomly on the grid
 
 
-board_game(board)
-
-# Prints one Battleship 'S', randomly on the board
+# Prints one Battleship 'S', randomly on the board.
+# Which is only revealed after a turn.
 random_row = randint(0, len(board))
 random_column = randint(0, len(board[0]))
 
-# Prints ship loction. To be removed.
-print(random_row, random_column)
-
-# Input from the user for row and column choice.
-guess_row = int(input("GUESS ROW:\n"))
-guess_column = int(input("GUESS COLUMN:\n"))
 
 if guess_row == random_row and guess_column == random_column:
     print("HIT. Marked with a '*' on the board")
@@ -96,9 +137,8 @@ else:
     print("The Battleship's position is marked with an 'S' on the board")
     print(data, "'s Board")
     board[random_row][random_column] = "S"
-    board[guess_row][guess_column] = "X" 
-    print(board_game(board)) 
+    board[guess_row][guess_column] = "X"
+    print(board_game(board))
 
 
-# def get_five_ships(ships) -> int:
-# for ship in ships:    
+board_game(board)
