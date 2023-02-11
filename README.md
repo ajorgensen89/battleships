8305d4d7d40111f3f0f9d25f40ed0b231c1ddc56
ajorgensen89 <ajorgensen89@gmail.com>
2023-02-11 13:09:57 +0000

    Loop tried to loop. Computer trying to crash so commit changes

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 Making a choice.
 https://www.codecademy.com/courses/learn-python/lessons/battleship/exercises/welcome-to-battleship
 
+Love Sandwiches Coursework.
+https://p3-battleships.herokuapp.com/
+
 Trial and Errors.<br>
 Tried using Append() method to add a HIT or MISS to the Battleship Board but it was not working correctly as I had not identified the user's guess for row and column are in separate lists. It lead to it just being added to the list and not changing the current "o" character which makes the hidden board.<br>
 ![Append atempt](images/readme-images/Append.board.png)
--- run.py
+++ run.py
@@ -9,15 +9,19 @@ def get_name_input():
     This can be numbers, letters or special characters.
     For example: Player1 or Bobby. Even @Bobby_BattleShip-game!
     """
+    # Welcoming message with a few instructions to play.
+    print("=" * 30)
+    print("WELCOME TO BATTLESHIPS")
+    print("Select a row: 0 to 4")
+    print("and")
+    print("Select a column: 0 to 4")
+    print("=" * 30)
+    print("A HIT is marked with - '*'")
+    print("A MISS is marked with - 'X'")
+    print("If you sink a Battleship, it will be marked with - 'S'")
+    print("~" * 30)
+    # Create input field for a user to create a name or use their own.
     while True:
-        # Welcoming message with a few instructions to play.
-        print("="*25)
-        print("WELCOME TO BATTLESHIPS")
-        print("Select a row: 0 to 4")
-        print("and")
-        print("Select a column: 0 to 4")
-        print("=" * 25)
-        # Create input field for a user to create a name or use their own.
         name_input = input("Enter a name:")
         name = name_input
         # If a valid input is filled in, break from while loop and continue
@@ -41,12 +45,12 @@ def validate_input(value):
     try:
         if not value:
             raise EOFError(
-                "Not Valid... You need to enter a name."
+                "It's empty... You need to enter a name."
             )
     except EOFError as e_err:
         print(f"Your input caused an error: {e_err} Try again.")
         return False
-
+    
     return True
 
 # Saved name input in 'data' variable.
@@ -56,15 +60,23 @@ data = get_name_input()
 
 # Create empty list to store the game's board.
 board = []
-
+ai = []
+guess_row, guess_column = [], []
+ai_rand_row, ai_rand_col = [], []
 # Create a list of 5 lists, all 5 "o" characters long. Loop and add in to
 # the empty board variable above.
 for x in range(5):
     board.append(["o"] * 5)
+    ai.append(["."] * 5)
 # Define a Battleship Board function in board_game.
+# def computer_board(ai):
+#    """
+#    AI turn and Battleship board
+#    """
+MISS = "X"
 
 
-def board_game(board):
+def board_game(board, ai):
     """
     Create a function that combines the above 5 lists into a 5x5 grid shape
     and remove decoration.
@@ -73,55 +85,93 @@ def board_game(board):
     for row in board:
         print((" ").join(row))
 
+    for row in ai:
+        print((" ").join(row))
 
-random_row = randint(1, len(board)) - 1
-random_column = randint(1, len(board[0])) - 1
+
+random_row, random_column = randint(1, len(board)) - 1, randint(1, len(board)) - 1
+ai_rand_row, ai_rand_col = randint(1, len(ai)) - 1, randint(1, len(ai)) - 1
 
 # For loop, to play the game on the grid created.
 
+
 for battle in range(5):
-    guess_row = int(input("GUESS ROW:\n"))
-    guess_column = int(input("GUESS COLUMN:\n"))
-    try:
-        if guess_row and guess_column < 0 or guess_row and guess_column > 5:
-            print("Re enter a valid row and column number between 0-4")
-            guess_row = int(input("GUESS ROW:\n"))
-            guess_column = int(input("GUESS COLUMN:\n"))
-    except ValueError:
-        print("error")
-    if guess_row == random_row and guess_column == random_column:
-        print("HIT. Marked with a '*' on the board")
-        print(f"The Battleship sunk on Row:{guess_row}")
-        print(f"Column:{guess_column}")
-        print(data, "'s Board")
-        board[guess_row][guess_column] = "*"
-        print(board_game(board))
-        break
-    else:
+    guess_row, guess_column = int(input("GUESS ROW:\n")), int(input("GUESS COLUMN:\n"))
+    ai_guess_r, ai_guess_c = randint(1, len(ai)) - 1, randint(1, len(ai)) - 1
+    if guess_row and guess_column == random_row and random_column:
         print("MISSED. Marked with an 'X' on the board")
-        print(f"It was on Row:{random_row} and Column:{random_column}")
+        print("Battleship is marked with an 'S'")
+        board[guess_row][guess_column] = MISS
+        print("Batttleship was on:")
+        print(f"Row:{random_row} Column:{random_column}")
+        print("MISSED. Marked with an 'X' on the board")
+        print(f"Ai Battleship was on Row:{ai_rand_row} and Column:{ai_rand_col}")
         print("Battleship is marked with an 'S'")
         print(data, "'s Board")
-        board[guess_row][guess_column] = "X"
-        print(f"Batttleship was on Row:{random_row}Column:{random_column}") 
-        print(board_game(board))
-    if guess_row and guess_column == str("X"):
+        print(board_game(board, ai))
+    if ai_guess_r and ai_guess_c == ai_rand_row and ai_rand_col:
+        print("MISSED. Marked with an 'X' on the board")
+        print("Battleship is marked with an 'S'")
+        board[guess_row][guess_column] = MISS
+        print("Batttleship was on:")
+        print(f"Row:{random_row} Column:{random_column}")
+        print("MISSED. Marked with an 'X' on the board")
+        print(f"Ai Battleship was on Row:{ai_rand_row} and Column:{ai_rand_col}")
+        print("Battleship is marked with an 'S'")
+        print(data, "'s Board")
+        print(board_game(board, ai))   
+    if ai_guess_r == ai_rand_row and ai_guess_c == ai_rand_col:
+        print("HIT. Marked with a '*' on the board")
+        print("The Battleship sunk on:")
+        print(f"Row:{ai_guess_r} Column:{ai_guess_c}")
+        ai[ai_guess_r][ai_guess_c] = "*"
+        print(data, "'s Board")
+        print(board_game(board, ai))
+        break
+    if guess_row == random_row and guess_column == random_column:
+        print("Computer WINS!")
+        print("HIT. Marked with a '*' on the board")
+        print("The Battleship sunk:")
+        print(f"Row:{guess_row}")
+        print(f"Column:{guess_column}")
+        board[guess_row][guess_column] = "*"
+        print(data, "'s Board")
+        print(board_game(board, ai))
+        break
+    elif guess_row and guess_column == MISS:
         print("You have guessed that elready. Try again.")
-        guess_row = int(input("GUESS ROW:\n"))
-        guess_column = int(input("GUESS COLUMN:\n"))
+        guess_row, guess_column = int(input("GUESS ROW:\n")), int(input("GUESS COLUMN:\n"))
+    elif ai_guess_r and ai_guess_c == MISS:
+        print("You have guessed that elready. Try again.")
+        ai_guess_r, ai_guess_c = randint(1, len(ai)) - 1, randint(1, len(ai)) - 1    
+     
 
-print("This", battle)
 
-
-def game_over(board):
+def game_over(board, ai):
 
     print("Game Over. You did not sink the Battleship")
-     
-    board[random_row][random_column] = "S"
     print(data, "'s Board")
-    print(board_game(board))
+    # print(board_game(data, board))
+    print("Computer's Board")
+    # print(board_game("Computer", ai))
 
     return board
 
 
-gameOver = game_over(board)
+game_over(board, ai)
+
+
+def validate_grid(user, comp):
+    """
+    Validated input for Row and Column values
+    """
+    try:
+        if not user and not comp:
+            raise ValueError(
+                "Incorrect co-ordinates."
+            )
+    except ValueError as v_err:
+        print(f"Wrong. {v_err} Try again.")
+        return False
+
+    return True    
