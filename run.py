cdc769eb3f31919f4ff0ffec288af733bbc96ec8
ajorgensen89 <ajorgensen89@gmail.com>
2023-02-12 18:25:01 +0000

    Attempted to add validing functions for guess inputs. Only validating it once at the moment. Each code loop added to bottom - computer issues. Savind ALL work

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 from random import randint
 # Import file above for random integer production.
+global guess_row
+global guess_column
+global guessedRowColumn
 
 
 def get_name_input():
@@ -13,9 +16,11 @@ def get_name_input():
     # Welcoming message with a few instructions to play.
     print("=" * 40)
     print("WELCOME TO BATTLESHIPS")
-    print("Guess a row between: 0 to 4")
+    print("Guess a ROW between: 0 to 4")
+    print("ROWS are left most row(0), to right most row(4)")
     print("and")
-    print("Guess a column between: 0 to 4")
+    print("Guess a COLUMN between: 0 to 4")
+    print("COLUMNS are top column(0), to bottom column(4)")
     print("=" * 40)
     print("A HIT is marked with - '*'")
     print("A MISS is marked with - 'X'")
@@ -32,8 +37,7 @@ def get_name_input():
         # If a valid input is filled in, break from while loop and continue
         # with the Battleship game.
         if validate_input(name):
-            print_name = name + "'s board!"
-            print(print_name)
+            print("Good Luck " + name + "!")
             break
     # Return name value, to function, to be used to label the user's board.
     # Saved in variable - data.
@@ -53,7 +57,7 @@ def validate_input(value):
                 "It's empty... You need to enter a name."
             )
     except EOFError as e_err:
-        print(f"Your input caused an error: {e_err} Try again.")
+        print(f"Your input caused an error: {e_err}  Try again.")
         return False
 
     return True
@@ -62,6 +66,41 @@ def validate_input(value):
 
 
 data = get_name_input()
+
+
+def player_guess():
+    """
+    To validate guess for row and column by user.
+    """
+    while True:
+        # Input from the user for row and column choice.
+        guess_row = int(input("\nGUESS ROW:\n"))
+        guess_column = int(input("GUESS COLUMN:\n"))
+        row = guess_row
+        column = guess_column
+        if validate_player_guess(row, column):
+            print(validate_player_guess(row, column))
+            print("CORRECT INTEGER RANGE...")
+            break
+
+    return row, column
+
+
+def validate_player_guess(guessed_row, guessed_column):
+    """
+    Errors for grid ranges. Raise error if out of range of the board length.
+    """
+    try:
+        if guessed_row and guessed_column < 0 or guessed_row and guessed_column >= 5:
+            raise ValueError(
+                "Out of grid range! Try again."
+            )
+    except ValueError as v_err:
+        print(f"Oops. {v_err}")
+        return False
+
+    return True
+
 
 # Create empty list to store the game's board Redefine in function.
 board = []
@@ -97,20 +136,48 @@ def board_game(board):
 random_row = randint(1, len(board)) - 1
 random_column = randint(1, len(board[0])) - 1
 
-guess_row = []
-guess_column = []
 # For loop, to play the game on the grid created.
 
 for battle in range(5):
-    guess_row = int(input("GUESS ROW:\n"))
-    guess_column = int(input("GUESS COLUMN:\n"))
-    if guess_row and guess_column < 0 or guess_row and guess_column >= 5:
+    # guess_row = int(input("\nGUESS ROW:\n"))
+    # guess_column = int(input("GUESS COLUMN:\n"))    
+    guessedRowColumn = player_guess()    
+    print(guessedRowColumn)
+    
+    if guessedRowColumn == random_row and random_column:
+        print("HIT!")
+        print("You found it!")
+        print("The Battleship sunk on:")
+        print(f"Co-ordinates: {guessedRowColumn}")
+        print(data, "'s Board")
+        guessedRowColumn = "X"
+        board_game(board)
+    elif guessedRowColumn == "X":
+        print("You have guessed that already. Try again.")
+    else:
+        print("MISSED... Try again...")
+        print(f"Battleship was on Row:{random_row} Column:{random_column}")
+        print(data, "'s Board")
+        board[guess_row][guess_column] = "X"
+        board_game(board)
+    if battle == 4:
+        board[random_row][random_column] = "S"
+        print("The Battleship was too well hidden.. GAME OVER.")
+        board_game(board)
+
+for battle in range(5):
+    # guess_row = int(input("\nGUESS ROW:\n"))
+    # guess_column = int(input("GUESS COLUMN:\n"))
+    print(guessedRc)
+    if guess_row or guess_column < 0 and guess_row or guess_column >= 5:
         print("Re enter a valid row and column number between 0-4")
         guess_row = int(input("GUESS ROW:\n"))
         guess_column = int(input("GUESS COLUMN:\n"))
     if guess_row == random_row and guess_column == random_column:
         print("HIT!")
-        print(f"You sunk the Battleship sunk on Row:{guess_row}")
+        print("You found it!")
+        print("The Battleship sunk on:")
+        print(f"Row:{guess_row}")
         print(f"Column:{guess_column}")
         print(data, "'s Board")
         board[guess_row][guess_column] = "*"
@@ -120,12 +187,11 @@ for battle in range(5):
         print("You have guessed that already. Try again.")
     else:
         print("MISSED... Try again...")
-        print(f"It was on Row:{random_row} and Column:{random_column}")
+        print(f"Battleship was on Row:{random_row} Column:{random_column}")
         print(data, "'s Board")
         board[guess_row][guess_column] = "X"
-        print(f"Batttleship was on Row:{random_row}Column:{random_column}")
         board_game(board)
     if battle == 4:
         board[random_row][random_column] = "S"
         print("The Battleship was too well hidden.. GAME OVER.")
-        board_game(board)
+        board_game(board)        
