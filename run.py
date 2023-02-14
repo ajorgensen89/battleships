3f4806b5d86befa2b9f867db50d85ceafb94256d
ajorgensen89 <ajorgensen89@gmail.com>
2023-02-14 17:09:09 +0000

    Checking Linter and adjusting code. Adding comments and docstrings

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
+'''
+Import randint from Random Module
+to use for random number generation.
+'''
 from random import randint
-# Import file above for random integer production.
+
+# Starting the Battleship game inside a function so game can be
+# played again after completing game.
+# 'y' for yes, to continue.
+# Exit game using this function 'n' for no.
+
 
 # Created functions, with credit, from Love Sandwiches Walkthrough
 # via the course.
@@ -7,66 +16,72 @@ from random import randint
 # before continuing into the game.
 
 
-def want_to_play_again():
-
-    def get_name_input():
-        """
-        Create name for user's Battleship Board.
-        Run while loop to check user had entered a name for their boardgame.
-        Break loop if valid 'input' has been added. Continue with the game.
-        This can be numbers, letters or special characters.
-        For example: Player1 or Bobby. Even @Bobby_BattleShip-game!
-        """
-        # Welcoming message with a few instructions to play.
-        print("=" * 40)
-        print("WELCOME TO BATTLESHIPS")
-        print("Guess a ROW between: 0 to 4")
-        print("ROWS are left most row(0), to right most row(4)")
-        print("and")
-        print("Guess a COLUMN between: 0 to 4")
-        print("COLUMNS are top column(0), to bottom column(4)")
-        print("=" * 40)
-        print("A HIT is marked with - '*'")
-        print("A MISS is marked with - 'X'")
-        print("You have 5 turns to guess correct.\n")
-        print("Keep track of your guesses as they count towards a turn..\n")
-        print("The AI(Computer) will be one step closer to winning..")
-        print("Get GAME OVER and...")
-        print("The Battleship's position is mark with an - 'S'")
-        print("The players board will be the top board displayed.")
-        print("The board underneathe, will belong to the Ai.")
-        print("~" * 40)
-        # Create input field for a user to create a name or use their own.
-        while True:
-            name_input = input("Enter a name: ")
+def get_name_input():
+    """
+    Create name for user's Battleship Board.
+    Run while loop to check user had entered a name for their boardgame.
+    Break loop if valid 'input' has been added. Continue with the game.
+    This can be numbers, letters or special characters.
+    For example: Player1 or Bobby. Even @Bobby_BattleShip-game!
+    """
+    # Welcoming message with a few instructions to play.
+    print("=" * 40)
+    print("WELCOME TO BATTLESHIPS")
+    print("Guess a ROW between: 0 to 4")
+    print("ROWS are left most row(0), to right most row(4)")
+    print("and")
+    print("Guess a COLUMN between: 0 to 4")
+    print("COLUMNS are top column(0), to bottom column(4)")
+    print("=" * 40)
+    print("A HIT is marked with - '*'")
+    print("A MISS is marked with - 'X'")
+    print("You have 5 turns to guess correct.\n")
+    print("Keep track of your guesses as they count towards a turn..\n")
+    print("The AI(Computer) will be one step closer to winning..")
+    print("Get GAME OVER and...")
+    print("The Battleship's position is mark with an - 'S'")
+    print("The players player_board will be the top player_board displayed.")
+    print("The player_board underneathe, will belong to the Ai.")
+    print("~" * 40)
+    # Create input field for a user to create a name or use their own.
+    while True:
+        name_input = input("Enter a name: ")
+        name = name_input
+        # If a valid input is filled in, break from while loop and continue
+        # with the Battleship game.
+        if validate_input(name):
             name = name_input
-            # If a valid input is filled in, break from while loop and continue
-            # with the Battleship game.
-            if validate_input(name):
-                name = name_input
-                break
-        # Return name value, to function, to be used to label the user's board.
-        # Saved in variable - data.
-        return name
+            break
+    # Return name value, to function, to be used to label the user's board.
+    # Saved in variable - data.
+    return name
 
-    def validate_input(value):
-        """
-        Validates user input for name.
-        Raises Error if user has not added a name.
-        Check for input in try:
-        Uses except: To print out Error.
-        """
-        try:
-            if not value:
-                raise EOFError(
-                    "It's empty... You need to enter something here."
-                )
-        except EOFError as e_err:
-            print(f"Your input caused an error: {e_err} Try again.")
-            return False
 
-        return True
+def validate_input(value):
+    """
+    Validates user input for name.
+    Raises Error if user has not added a name.
+    Check for input in try:
+    Uses except: To print out Error.
+    """
+    try:
+        if not value:
+            raise EOFError(
+                "It's empty... You need to enter something here."
+            )
+    except EOFError as e_err:
+        print(f"Your input caused an error: {e_err} Try again.")
+        return False
 
+    return True
+
+
+def want_to_play_again():
+    """
+    Function to restart game at the end. Called at the end inside
+    a while loop to repeat the game depending on input of
+    'y' for yes, to play again, and 'n' for no, to exit the program.
+    """
     # Two functions created to help with validing input from the user.
     # Credit for layout from - 101 computing.net. In Credit for README.md file
     # Adaptations made to the code where necessary.
@@ -136,28 +151,28 @@ def want_to_play_again():
             break
 
     # Create empty list to store the game's board Redefine in function.
-    board = []
-    ai = []
+    player_board = []
+    ai_board = []
     # Create a list of 5 lists, all 5 "o" characters long. Loop and add in to
-    # the empty board variable above.
+    # the empty player_board variable above.
     for x in range(5):
-        board.append(["o"] * 5)
-        ai.append(["."] * 5)
+        player_board.append(["o"] * 5)
+        ai_board.append(["."] * 5)
 
-    def battleship_game(board, ai):
+    def battleship_game(player_board, ai_board):
         """
         Create a function that combines the above 5 lists into a 5x5 grid shape
         and remove decoration.
         Join list with a 'space' between the "o" characters.
         """
-        for row in board:
+        for row in player_board:
             print((" ").join(row))
 
-        for row in ai:
+        for row in ai_board:
             print((" ").join(row))
 
-    ai_s_row = randint(1, len(board)) - 1
-    ai_s_col = randint(1, len(board[0])) - 1
+    ai_s_row = randint(1, len(player_board)) - 1
+    ai_s_col = randint(1, len(player_board[0])) - 1
 
     while True:
         print("\nFIRST...")
@@ -181,8 +196,8 @@ def want_to_play_again():
         validate_input("blah:")
         guess_r = input_row("GUESS ROW:\n")
         guess_c = input_column("GUESS COLUMN:\n")
-        ai_guess_r = randint(1, len(board)) - 1
-        ai_guess_c = randint(1, len(board[0])) - 1
+        ai_guess_r = randint(1, len(player_board)) - 1
+        ai_guess_c = randint(1, len(player_board[0])) - 1
         while True:
             if (guess_r < 0 or guess_c < 0) or (guess_r >= 5 or guess_c >= 5):
                 print("Re enter a valid row and column number between 0-4")
@@ -198,42 +213,42 @@ def want_to_play_again():
             print(f"Row:{guess_r}")
             print(f"Column:{guess_c}\n")
             print(data, "'s Board!")
-            ai[guess_r][guess_c] = "*"
-            battleship_game(board, " ")
-            battleship_game(" ", ai)
+            ai_board[guess_r][guess_c] = "*"
+            battleship_game(player_board, " ")
+            battleship_game(" ", ai_board)
             print("AI's Board!")
             print("In", tally, "turns!")
             break
         if ai_guess_r == ship_row and ai_guess_c == ship_col:
             print("A! HIT your ship!")
-            board[ai_guess_r][ai_guess_c] = "*"
-            ai[guess_r][guess_c] = "X"
-            battleship_game(board, " ")
-            battleship_game(" ", ai)
+            player_board[ai_guess_r][ai_guess_c] = "*"
+            ai_board[guess_r][guess_c] = "X"
+            battleship_game(player_board, " ")
+            battleship_game(" ", ai_board)
             tally += 1
             print("AI has Won this time.. in", tally, "goes!")
             break
-        elif ai[guess_r][guess_c] == "X":
+        elif ai_board[guess_r][guess_c] == "X":
             print("~" * 40)
             print("Nice try but you have guessed that already. Try again...")
-        elif board[ai_guess_r][ai_guess_c] == "X":
+        elif player_board[ai_guess_r][ai_guess_c] == "X":
             print("The AI already tried that")
-            ai_guess_r = randint(1, len(board)) - 1
-            ai_guess_c = randint(1, len(board[0])) - 1
-            ai[guess_r][guess_c] = "X"
-            board[ai_guess_r][ai_guess_c] = "X"
-            battleship_game(board, " ")
-            battleship_game(" ", ai)
+            ai_guess_r = randint(1, len(player_board)) - 1
+            ai_guess_c = randint(1, len(player_board[0])) - 1
+            ai_board[guess_r][guess_c] = "X"
+            player_board[ai_guess_r][ai_guess_c] = "X"
+            battleship_game(player_board, " ")
+            battleship_game(" ", ai_board)
             tally += 1
         else:
             print("MISSED... Try again...")
             print(f"Batttleship was on Row:{ai_s_row} Column:{ai_s_col}\n")
             print(data, "'s turn:", tally)
             print(data, "'s Board")
-            ai[guess_r][guess_c] = "X"
-            board[ai_guess_r][ai_guess_c] = "X"
-            battleship_game(board, " ")
-            battleship_game(" ", ai)
+            ai_board[guess_r][guess_c] = "X"
+            player_board[ai_guess_r][ai_guess_c] = "X"
+            battleship_game(player_board, " ")
+            battleship_game(" ", ai_board)
             print("AI's Board!")
             tally += 1
 
